0d025097a392652580d82df67d27c18fb729bb66
Pratik Luitel <pluitel11@gmail.com>
2022-01-28 21:39:18 +0545

    some changes

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -4,12 +4,13 @@ import json
 
 # open the formatted and unformatted data
 try:
-    with open('../data/formatted_data.json','r') as f:
+    with open('/home/pratik/Projects/EQ map/data/formatted_data.json','r') as f:
         data = json.load(f)
-    with open('../data/raw_geojson.json','r') as fg:
+    with open('/home/pratik/Projects/EQ map/data/raw_geojson.json','r') as fg:
         geojson_data = json.load(fg)
 except:
     print('Could not find data json file. Make sure data is in proper format in formatted_data.json')
+    raise SystemExit
 
 magnitudes = [round(v['magnitude']) if v['magnitude'] is not None else 0 for v in data.values()]
 times = [datetime.datetime.strptime(v['time'].split('+')[0],'%Y-%m-%d %H:%M:%S') for v in data.values()]
