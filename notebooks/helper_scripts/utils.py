d0b51c49b621e4fe8730c9913018330be04e67d1
Pratik Luitel <pluitel11@gmail.com>
2022-02-03 23:04:41 +0545

    tried adding dynamic labelling, not fully integrated

--- notebooks/helper_scripts/utils.py
+++ notebooks/helper_scripts/utils.py
@@ -1,5 +1,6 @@
 from typing import Callable
 import matplotlib.pyplot as plt
+import matplotlib.figure as figure
 
 def annotate_polygons(ax: plt.Axes, **kwarg_dict) -> Callable:
     """
@@ -23,7 +24,19 @@ def annotate_polygons(ax: plt.Axes, **kwarg_dict) -> Callable:
 
     def annotate_polygon(x):
         text = x[annotation_field]+f'\n{x[annotation_value_field]}'
-        polygon_center_coords = x.geometry.centroid.coords[0]
+        polygon = x.geometry
+        print(polygon)
+        min_bounding_rect_width = abs(polygon.envelope.exterior.coords.xy[0][1] - polygon.envelope.exterior.coords.xy[0][0])
+
+        annotation_text = ax.text(polygon.centroid.coords[0][0], polygon.centroid.coords[0][1],text,fontsize=9)
+        annotation_text_width = annotation_text.get_window_extent(renderer=ax.figure.canvas.get_renderer()).width
+
+        del ax.texts[-1]
+
+        print(min_bounding_rect_width, annotation_text_width)
+
+        polygon_center_coords = polygon.centroid.coords[0]
         applied_color = highlight_color if x[annotation_value_field] > threshold else color
-        return ax.annotate(text=text, xy=polygon_center_coords, color=applied_color, ha='center')
+        return ax.annotate(text=text, xy=polygon_center_coords, xytext=polygon_center_coords,
+            color=applied_color, ha='center', fontsize=9)
     return annotate_polygon
