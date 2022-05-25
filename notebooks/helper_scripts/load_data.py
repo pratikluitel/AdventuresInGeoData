ef5d381c14560bdb9da00e730791880f260667a4
Pratik Luitel <pluitel11@gmail.com>
2022-05-25 23:32:23 +0545

    added ktm election

--- notebooks/helper_scripts/load_data.py
+++ notebooks/helper_scripts/load_data.py
@@ -2,22 +2,26 @@ import requests
 import datetime
 import json
 
-data = requests.get('https://earthquake.usgs.gov/fdsnws/event/1/query?\
-    format=geojson&minlatitude=20&maxlatitude=35&minlongitude=75&maxlongitude=95&starttime=100-01-01').json()
+data = requests.get(
+    "https://earthquake.usgs.gov/fdsnws/event/1/query?\
+    format=geojson&minlatitude=20&maxlatitude=35&minlongitude=75&maxlongitude=95&starttime=100-01-01"
+).json()
 
 formatted_data = {}
 
-for feature in data['features']:
-    id, properties, geometry = feature['id'], feature['properties'], feature['geometry']
-    country = properties['place'].split(',')[-1].strip()
+for feature in data["features"]:
+    id, properties, geometry = feature["id"], feature["properties"], feature["geometry"]
+    country = properties["place"].split(",")[-1].strip()
 
-    if country == 'Nepal':
+    if country == "Nepal":
         formatted_data[id] = {
-            'magnitude': properties['mag'],
-            'place': properties['place'],
-            'time': datetime.datetime.fromtimestamp(int(properties['time']/1000), tz=datetime.timezone.utc),
-            'coordinates':geometry['coordinates'][:2]
+            "magnitude": properties["mag"],
+            "place": properties["place"],
+            "time": datetime.datetime.fromtimestamp(
+                int(properties["time"] / 1000), tz=datetime.timezone.utc
+            ),
+            "coordinates": geometry["coordinates"][:2],
         }
 
-with open('../../data/formatted_data.json','w') as f:
+with open("../../data/formatted_data.json", "w") as f:
     json.dump(formatted_data, f, indent=3, default=str)
--- notebooks/helper_scripts/utils.py
+++ notebooks/helper_scripts/utils.py
@@ -1,57 +1,91 @@
 from typing import Callable, Tuple
 import matplotlib.pyplot as plt
 
-def offset(coord:Tuple, xoff:float=0, yoff:float=0) -> Tuple:
-    return (coord[0]+xoff, coord[1]+yoff)
 
-def transform_nepal(polygon_center_coords:Tuple, district:str) -> Tuple:
+def offset(coord: Tuple, xoff: float = 0, yoff: float = 0) -> Tuple:
+    return (coord[0] + xoff, coord[1] + yoff)
+
+
+def transform_nepal(polygon_center_coords: Tuple, district: str) -> Tuple:
     """
     STATIC TRANSFORMATION LOGIC - TO DELETE AFTER DYNAMIC LOGIC IS IMPLEMENTED
     """
     coords = polygon_center_coords
-    if district in ('SINDHULI','KAILALI','DAILEKH','SALYAN','KASKI','TANAHUN','MANANG','SIRAHA','ILAM'
-                    ,'LAMJUNG','BARDIYA','BANKE','DANG','KAPILBASTU','ROLPA','MUSTANG','SARLAHI'):
+    if district in (
+        "SINDHULI",
+        "KAILALI",
+        "DAILEKH",
+        "SALYAN",
+        "KASKI",
+        "TANAHUN",
+        "MANANG",
+        "SIRAHA",
+        "ILAM",
+        "LAMJUNG",
+        "BARDIYA",
+        "BANKE",
+        "DANG",
+        "KAPILBASTU",
+        "ROLPA",
+        "MUSTANG",
+        "SARLAHI",
+    ):
         coords = offset(coords, yoff=-0.04)
-    elif district in ('DARCHULA','KAVRE','JHAPA','BHOJPUR','DADELDHURA',
-                    'SAPTARI','BAJURA','JAJARKOT','RUKUM','MUGU',
-                    'GULMI','PYUTHAN','PALPA','NAWALPARASI','SAPTARI'):
+    elif district in (
+        "DARCHULA",
+        "KAVRE",
+        "JHAPA",
+        "BHOJPUR",
+        "DADELDHURA",
+        "SAPTARI",
+        "BAJURA",
+        "JAJARKOT",
+        "RUKUM",
+        "MUGU",
+        "GULMI",
+        "PYUTHAN",
+        "PALPA",
+        "NAWALPARASI",
+        "SAPTARI",
+    ):
         coords = offset(coords, yoff=-0.05)
-    elif district in ('NUWAKOT','DOLAKHA','TAPLEJUNG'):
+    elif district in ("NUWAKOT", "DOLAKHA", "TAPLEJUNG"):
         coords = offset(coords, yoff=-0.08)
-    elif district in ('SURKHET', 'KALIKOT','SINDHUPALCHOK'):
+    elif district in ("SURKHET", "KALIKOT", "SINDHUPALCHOK"):
         coords = offset(coords, yoff=-0.1)
-    elif district in ('BHAKTAPUR'):
-        coords = offset(coords,yoff=-0.01)
-    elif district in ('KANCHANPUR', 'MAKWANPUR'):
+    elif district in ("BHAKTAPUR"):
+        coords = offset(coords, yoff=-0.01)
+    elif district in ("KANCHANPUR", "MAKWANPUR"):
         coords = offset(coords, xoff=-0.03)
-    elif district in ('PARBAT','ARGHAKHANCHI'):
-        coords = offset(coords,xoff=0.04)
-    elif district in ('SOLUKHUMBU'):
+    elif district in ("PARBAT", "ARGHAKHANCHI"):
+        coords = offset(coords, xoff=0.04)
+    elif district in ("SOLUKHUMBU"):
         coords = offset(coords, yoff=-0.1, xoff=0.03)
-    elif district in ('KHOTANG','JUMLA','BAITADI','RASUWA','MYAGDI'):
+    elif district in ("KHOTANG", "JUMLA", "BAITADI", "RASUWA", "MYAGDI"):
         coords = offset(coords, xoff=-0.02, yoff=-0.05)
-    elif district in ('BAGLUNG'):
+    elif district in ("BAGLUNG"):
         coords = offset(coords, xoff=-0.1, yoff=-0.02)
-    elif district in ('UDAYAPUR'):
+    elif district in ("UDAYAPUR"):
         coords = offset(coords, xoff=-0.12, yoff=0.01)
-    elif district in ('DHADING'):
+    elif district in ("DHADING"):
         coords = offset(coords, yoff=-0.14, xoff=-0.08)
-    elif district in ('RAMECHHAP'):
+    elif district in ("RAMECHHAP"):
         coords = offset(coords, yoff=-0.14, xoff=-0.09)
-    elif district in ('SYANGJA'):
-        coords = offset(coords, xoff=0.02,yoff=-0.06)
-    elif district in ('BAJHANG','GORKHA','CHITWAN'):
+    elif district in ("SYANGJA"):
+        coords = offset(coords, xoff=0.02, yoff=-0.06)
+    elif district in ("BAJHANG", "GORKHA", "CHITWAN"):
         coords = offset(coords, xoff=0.04, yoff=-0.05)
-    elif district in ('RUPANDEHI'):
+    elif district in ("RUPANDEHI"):
         coords = offset(coords, xoff=0.03, yoff=-0.04)
-    elif district in ('OKHALDHUNGA'):
+    elif district in ("OKHALDHUNGA"):
         coords = offset(coords, xoff=0.02, yoff=-0.09)
 
     return coords
 
+
 def annotate_polygons(ax: plt.Axes, **kwarg_dict) -> Callable:
     """
-    inputs: 
+    inputs:
     ax - The axes to plot the annotation
     annotation_field - The text field to use for the annotation
     annotation_value_field - The value field to use for the annotation
@@ -62,21 +96,25 @@ def annotate_polygons(ax: plt.Axes, **kwarg_dict) -> Callable:
     returns a function that generates an annotation
     for a polygon on the given axes
     """
-    
-    annotation_field, annotation_value_field, threshold = kwarg_dict['annotation_field'] , kwarg_dict['annotation_value_field'], kwarg_dict['threshold']
 
-    small_polygon_map = kwarg_dict['small_polygon_map']
+    annotation_field, annotation_value_field, threshold = (
+        kwarg_dict["annotation_field"],
+        kwarg_dict["annotation_value_field"],
+        kwarg_dict["threshold"],
+    )
+
+    small_polygon_map = kwarg_dict["small_polygon_map"]
 
     try:
-        color, highlight_color = kwarg_dict['color'], kwarg_dict['highlight_color']
+        color, highlight_color = kwarg_dict["color"], kwarg_dict["highlight_color"]
     except KeyError:
-        color, highlight_color = 'black', 'white'
+        color, highlight_color = "black", "white"
 
     def annotate_polygon(x):
         """
-        Someday, this will be fully dynamic, 
+        Someday, this will be fully dynamic,
         handle all form of text and line intersections,
-        and work for all forms of polygon combinations, 
+        and work for all forms of polygon combinations,
         not just maps.
 
         Someday.
@@ -84,22 +122,28 @@ def annotate_polygons(ax: plt.Axes, **kwarg_dict) -> Callable:
         small_polygons_map = small_polygon_map.copy()
 
         # need these for dynamic text size and other stuff
-        apparant_width = plt.gcf().get_size_inches()[0]*ax.figure.dpi
-        width = 1200 if apparant_width<=1200 else apparant_width
+        apparant_width = plt.gcf().get_size_inches()[0] * ax.figure.dpi
+        width = 1200 if apparant_width <= 1200 else apparant_width
 
-        fontsize = width/192 if x[annotation_field] in small_polygons_map.keys() else width/236
+        fontsize = (
+            width / 192
+            if x[annotation_field] in small_polygons_map.keys()
+            else width / 236
+        )
 
         # Generate labels from A-Z if small districts are encountered
         if x[annotation_field] in small_polygons_map.keys():
             text = small_polygons_map[x[annotation_field]]
         else:
-            text = f'{x[annotation_field].title()}\n{x[annotation_value_field]}'
+            text = f"{x[annotation_field].title()}\n{x[annotation_value_field]}"
 
-        applied_color = highlight_color if x[annotation_value_field] > threshold else color
-        
+        applied_color = (
+            highlight_color if x[annotation_value_field] > threshold else color
+        )
+
         polygon = x.geometry
         polygon_center_coords = polygon.centroid.coords[0]
-        
+
         ################################## DYNAMIC TRANSFORMATION LOGIC ##################################################################
         # # The width of the rectangle bounding polygon
         # min_bounding_rect_width = abs(polygon.envelope.exterior.coords.xy[0][1] - polygon.envelope.exterior.coords.xy[0][0])
@@ -121,5 +165,13 @@ def annotate_polygons(ax: plt.Axes, **kwarg_dict) -> Callable:
         # To fix while adjusting for projections
         # polygon_center_coords = transform.transform_point(transformed_center[0], transformed_center[1], gcrs.ccrs.PlateCarree())
 
-        return ax.annotate(text=text, xy=transformed_center, color=applied_color, ha='center', fontsize=fontsize, weight='bold')
+        return ax.annotate(
+            text=text,
+            xy=transformed_center,
+            color=applied_color,
+            ha="center",
+            fontsize=fontsize,
+            weight="bold",
+        )
+
     return annotate_polygon
